672dd35d9d896b6ab1e4208c9fcbc7f2ebb179aa
Marllon Wendel <124008701+Marllon-Wendel2@users.noreply.github.com>
2024-07-31 02:16:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import cors from "cors";
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://club-books.vercel.app",
     credentials: true,
 }))
 app.use(express.json())
